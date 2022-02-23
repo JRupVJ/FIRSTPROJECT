fc21ed96bb130ea2724dafc4e2f7b430281fed18
ruphu <ruphu@10.0.0.172>
2022-02-23 12:43:47 -0500

    USER INPUT ADDED

--- src/Practise/FirstPro.java
+++ src/Practise/FirstPro.java
@@ -1,6 +1,9 @@
 package Practise;
 
+import java.util.*;
+
 public class FirstPro {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,6 +11,12 @@ public class FirstPro {
 		System.out.println("SYSO");
 		System.out.println("new statement");
 		System.out.println("new line");
+		
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter User Name");
+		String userName = myObj.nextLine();
+		System.out.println("User name is: " + userName);
+			
 	}
 
 }
