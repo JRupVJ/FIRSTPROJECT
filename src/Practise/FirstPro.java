8560446fd01f3690f1ee61e4d2f328046b2c82b1
ruphu <ruphu@10.0.0.172>
2022-02-24 11:42:25 -0500

    ArrayList update

--- src/Practise/FirstPro.java
+++ src/Practise/FirstPro.java
@@ -2,6 +2,7 @@ package Practise;
 
 import java.util.*;
 
+
 class DispSub{
 	public void dispName() {
 		System.out.println("Sub class: Name 1");
@@ -17,15 +18,19 @@ public class FirstPro extends DispSub{
 		System.out.println("SYSO");
 		System.out.println("new statement");
 		System.out.println("new line");
-		
+		//user input example
 		Scanner myObj = new Scanner(System.in);
 		System.out.println("Enter User Name: ");
 		String userName = myObj.nextLine();
 		System.out.println("User name is: " + " " + userName);
-		
+		//inhertiance example
 		FirstPro myMainObj = new FirstPro();
 		myMainObj.dispName();
-			
+		//arraylist example
+		ArrayList<String> cloudList = new ArrayList<String>();
+		cloudList.add("AWS");
+		cloudList.add("gcp");
+		System.out.println(cloudList);
 	}
 
 }
